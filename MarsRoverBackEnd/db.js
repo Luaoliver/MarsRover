454fb1fa79ec4ee96dd2a42b0cd933f552de8a04
Luana Silva <lulu-so@hotmail.com>
2023-05-10 20:20:35 -0300

    feat: configuring adonis

--- MarsRoverBackEnd/db.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('MarsRover', 'root', 'Luana123', {dialect: 'mariadb', host: 'localhost', port: 3306});
-//nome, usuário, senha, opções(bd, local)
- 
-module.exports = sequelize;
