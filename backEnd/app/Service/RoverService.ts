51c46028edf5e27a4027dd1dad3b5c41067c685d
Luana Silva <lulu-so@hotmail.com>
2023-05-12 13:19:05 -0300

    feat: starting motion validation

--- backEnd/app/Service/RoverService.ts
+++ backEnd/app/Service/RoverService.ts
@@ -1,21 +1,24 @@
-import Rover from 'App/Models/Rover'
 import { DirectionHandler } from './DirectionService'
 import { Direction } from 'Contracts/enum'
+import Rover from 'App/Models/Rover'
+import Plateau from 'App/Models/Plateau'
+import InvalidMovementException from 'App/Exceptions/InvalidMovementException'
 
 export default class RoverService {
   public async create(roverParams): Promise<Rover> {
+    const [plateau] = await Plateau.query().orderBy('id', 'desc').limit(1)
     const rover = new Rover()
-    rover.merge(roverParams)
+    rover.merge({ plateauId: plateau.id, ...roverParams })
     rover.currentDirection = rover.startDirection
     rover.currentPositionX = rover.startPositionX
     rover.currentPositionY = rover.startPositionY
 
-    this.processInstruction(rover)
+    this.processInstruction(rover, plateau)
     await rover.save()
     return rover
   }
 
-  private processInstruction(rover: Rover) {
+  private processInstruction(rover: Rover, plateau: Plateau) {
     const directionHandler = new DirectionHandler(rover.startDirection)
     const instructionArray = rover.instruction.split('')
     console.log(instructionArray)
@@ -28,7 +31,10 @@ export default class RoverService {
           rover.currentDirection = directionHandler.rotateLeft()
           break
         case 'M':
-          if (rover.currentDirection === Direction.N) {
+          if (
+            rover.currentDirection === Direction.N &&
+            rover.currentPositionY + 1 <= plateau.yAxis
+          ) {
             rover.currentPositionY++
           } else if (rover.currentDirection === Direction.E) {
             rover.currentPositionX++
@@ -36,6 +42,8 @@ export default class RoverService {
             rover.currentPositionY--
           } else if (rover.currentDirection === Direction.W) {
             rover.currentPositionX--
+          } else {
+            throw new InvalidMovementException('invalid moviment')
           }
           break
       }
--- backEnd/app/Service/RoverValidation.ts
+++ backEnd/app/Service/RoverValidation.ts
@@ -2,7 +2,6 @@ import { validator, schema, rules } from '@ioc:Adonis/Core/Validator'
 import { Direction } from 'Contracts/enum'
 
 export const roverSchema = schema.create({
-  plateauId: schema.number(),
   startPositionX: schema.number(),
   startPositionY: schema.number(),
   instruction: schema.string({ trim: true }, [rules.regex(/^[LMR]+$/)]),
